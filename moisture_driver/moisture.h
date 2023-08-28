cc1c2e8758480d88bf88ae909581e22c2df56135
Mert Kalkancı <mertkalkanci05@hotmail.com>
2023-08-28 15:51:53 +0300

    header file

--- moisture_driver/moisture.h
+++ moisture_driver/moisture.h
@@ -1,7 +1,8 @@
 #ifndef MOISTURE_H
 #define MOISTURE_H
 
-void init_sensor(int gpioPin);
+void init_sensor_gpionum(int gpioPin);
+void init_sensor_adcnum(int pin);
 int get_moisture_raw(int gpioPin);//SELECTS ADC PIN AND READS VALUE BE AWARE OF IT
 int get_moisture_percent(int adcPin);//SELECTS ADC PIN AND READS VALUE BE AWARE OF IT
 
